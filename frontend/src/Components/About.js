5c871533a122639cb1692eb6047574cc7134d68b
Nimesh Tank <nimesh.ytank@gmail.com>
2022-02-28 02:05:55 +0530

    Contact and About UI Added

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -1,7 +1,129 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
+import profilePic from '../Images/myImage.jpeg'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <div className='container mt-5 profile-container'>
+        <form action="" >
+
+          {/* Top profile part */}
+          <div className="row justify-content-center">
+            <div className="col-md-4 mt-5">
+              <img src={profilePic} alt="Profile-Pic" id='profile-pic' />
+            </div>
+            <div className="col-md-6 mt-5">
+              <div className="profile-heading">
+                <h5>Nimesh Tank</h5>
+                <h6>Full Stack Developer</h6>
+                <p className="profile-rating mt-3 mb-5">Rating: <span className="span-rating">1/10</span></p>
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                  <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+                  </li>
+                  <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Timeline</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-2 mt-5">
+              <input type="submit" className="profile-edit-btn btn btn-light" value='Edit Profile' />
+            </div>
+          </div>
+
+          {/* Profile Bottom Part */}
+          <div className="row justify-content-center  ">
+            {/* Left side  */}
+            <div className="col-md-4 mb-5">
+              <div className="profile-links mt-3">
+                <p>Social Links</p>
+                <a href="www.google.com">LinkedIn</a>
+                <a href="www.google.com">GitHub</a>
+              </div>
+            </div>
+
+            <div className="col-md-8 about-info mt-4">
+              <div className="tab-content profile-tab" id='myTabContent'>
+
+                {/* About Tab  */}
+                <div class="tab-pane fade show active mb-4" id="home" role="tabpanel" aria-labelledby="home-tab">
+                  <div className="row ">
+                    <div className="col-md-6 about-title">
+                      <label>User Id</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>12524875631</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 about-title">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Nimesh Tank</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 about-title">
+                      <label>Email</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>nimesh.ytank@gmail.com</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 about-title">
+                      <label>Phone</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>5481245879</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 about-title">
+                      <label>Profession</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Full Stack Developer</p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Timeline Tab */}
+                <div class="tab-pane fade mb-4" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                  <div className="row">
+                    <div className="col-md-6 about-title">
+                      <label>Experience</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Beginer</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 about-title">
+                      <label>Hourly Rate</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>5$</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 about-title">
+                      <label>Total Projects</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>10</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
+
--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     <>
       {/* contact info grid */}
       <div className="contact-info mt-5">
-        <div className="container">
+        <div className="container ">
           <div className="row justify-content-around">
             <div className="col-lg-10  d-flex justify-content-between ">
               <div className="contact-info-item d-flex justify-content-start align-items-center">
@@ -49,10 +49,36 @@ export default function Contact() {
       </div>
 
       {/* contact us form */}
-      
+      <div className="contact-form">
+        <div className="container  my-5">
+          <div className="row justify-content-around">
+            <div className="col-lg-9 form-container">
+              <div className="contact-form-container m-5">
+                <div className="contact-form-title fw-bold fs-3">
+                  Contact Us
+                </div>
+                <form id="contact-form">
+                  <div className="contact-form-inputs d-flex justify-content-between my-3">
+                    <input type="text" className="form_input p-1 " placeholder='Your Name' required='true' />
+                    <input type="email" className="form_input p-1" placeholder='Your Email' required='true' />
+                    <input type="text" className="form_input p-1" placeholder='Your Mobile Number' required='true' />
+                  </div>
+                  <div className="contact-form-textarea mt-5">
+                    <textarea className="text_field_contact " row='10' cols='30' placeholder='Message' ></textarea>
+                  </div>
+                  <div className="contact-form-button mt-4">
+                    <button className="btn  btn-dark contact-submit-button">Send Message</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
+
 
 
 
